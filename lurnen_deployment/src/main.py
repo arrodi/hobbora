5687a26ba5e180a483feff818c0d517825d34b11
Artem Rodionov <artem.rod98@gmail.com>
2024-07-21 22:41:00 -0500

    cluster setup

---
+++ lurnen_deployment/src/main.py
@@ -0,0 +1,22 @@
+# STL IMPORTS
+from os import environ
+
+# EXT IMPORTS
+from flask import Flask, render_template
+from waitress import serve
+
+# FLASK INIT
+app = Flask(__name__)
+
+#########################
+##### SERVER ROUTES #####
+#########################
+@app.route("/", methods=['GET'])
+def hello_world():
+    print("Someone visited!")
+    return render_template("home.html")
+
+#########################
+##### SERVER BEGIN! #####
+#########################
+serve(app, host='0.0.0.0', port=int(environ["APP_PORT"]))
