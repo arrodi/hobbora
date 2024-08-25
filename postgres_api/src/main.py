4ddc64a3c17a0d5d4306b899757ffbcaeb1c7f36
Artem Rodionov <artem.rod98@gmail.com>
2024-08-25 16:05:43 -0500

    postgres init

---
+++ postgres_api/src/main.py
@@ -0,0 +1,20 @@
+import psycopg2
+from os import environ
+
+connection = psycopg2.connect(database=environ["POSTGRES_DB"], user=environ["POSTGRES_USER"], password=environ["POSTGRES_PASSWORD"], host=environ["POSTGRES_HOST"], port=environ["POSTGRES_PORT"])
+
+cursor = connection.cursor()
+
+sql_context ="""
+select 
+    *
+from 
+    customers
+"""
+
+cursor.execute(sql_context)
+
+# Fetch all rows from database
+record = cursor.fetchall()
+
+print("Data from Database:- ", record)
