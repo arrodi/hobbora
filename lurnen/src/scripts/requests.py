4828ff6531cdbe7c2167183c45721cff2e153501
Artem Rodionov <artem.rod98@gmail.com>
2024-09-04 22:52:30 -0500

    Crazy sign up update

--- lurnen/src/scripts/requests.py
+++ lurnen/src/scripts/requests.py
@@ -8,9 +8,6 @@ def get(api_url, api_endpoint):
 
 def post(api_url, api_endpoint, json_data):
 
-    print(f'{api_url}/{api_endpoint}')
-    print(json_data)
-    
     response = requests.post(f'{api_url}/{api_endpoint}', json=json_data)
 
     return response.status_code == 200
