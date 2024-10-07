1e7779fcd5ce30799e2b6dac5f82005c32e2bda5
Artem Rodionov <artem.rod98@gmail.com>
2024-10-06 20:49:51 -0500

    Cleaner App Init

--- microservices/web-ui/src/scripts/settings.py
+++ microservices/web-ui/src/scripts/settings.py
@@ -7,9 +7,6 @@ class Settings:
         
         self.api_url=os.environ["API_URL"]
 
-        current_directory = os.listdir()
-        print(current_directory)
-
         self.default_pictures_urls = self._get_all_paths_in_folder("static/images/default_hobbies")
 
     def _get_all_paths_in_folder(self, directory):
